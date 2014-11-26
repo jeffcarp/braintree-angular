3318e5aeb0d9f2000ee204a727466bcf3baa3a14
Jeff Carpenter <gcarpenterv@gmail.com>
2014-11-26 01:10:10 +0000

    Trim test runner output

--- spec/support/runner.js
+++ spec/support/runner.js
@@ -7,7 +7,7 @@ var server = app.listen(3001, '0.0.0.0', function() {
   var child = spawn('npm', ['run', 'integration']);
 
   child.stdout.on('data', function(chunk) {
-    console.log(chunk.toString('utf8'));
+    console.log(chunk.toString('utf8').trim());
   });
   child.stdout.on('end', function() {
     server.close();
