c3ec23ae525e9c2f6e2ae5353aa61ddefc0ee1cb
Jeff Carpenter <gcarpenterv@gmail.com>
2014-12-09 19:14:42 +0000

    Remove karma, integration test with bare phantomjs

---
+++ spec/integration/dropin-spec.js
@@ -0,0 +1,49 @@
+var phantom = require('phantom');
+var assert = require('assert');
+var app = require('../support/app');
+console.log('ok testing');
+var baseURL = 'http://localhost:3001';
+
+describe('Drop-in', function() {
+
+  var ph;
+  var server;
+  this.timeout(1e4);
+
+  before(function(done) {
+    phantom.create(function(phInstance) {
+      ph = phInstance;
+      server = app.listen(3001, function() {
+        done();
+      });
+    });
+  });
+
+  it('adds elements to the page on setup', function(done) {
+    ph.createPage(function(page) {
+
+      page.onConsoleMessage(function(msg) {
+        console.log('page console -> ', msg);
+      });
+
+      page.open(baseURL+'/test-dropin.html', function(status) {
+        page.evaluate(function() { return document.documentElement.innerHTML; }, function(result) {
+
+          assert(result.indexOf('iframe') !== -1, 'adds an iframe to the page');
+          var input = '<input type="hidden" name="payment_method_nonce">';
+          assert(result.indexOf(input) !== -1, 'adds nonce input');
+
+          done();
+        });
+      });
+    });
+  });
+
+  after(function(done) {
+    ph.exit();
+    server.close();
+    done();
+  });
+});
+
+
--- spec/support/app.js
+++ spec/support/app.js
@@ -1,10 +1,26 @@
-var app = require('express')();
+var express = require('express');
+var fs = require('fs');
+var app = express();
+
+app.use(express.static(__dirname));
+
+app.get('/braintree-angular.js', function(req, res) {
+  var path = __dirname+'/../../dist/braintree-angular.js';
+  res.send(fs.readFileSync(path, 'utf8'));
+});
+
+app.get('/angular.js', function(req, res) {
+  var path = __dirname+'/../../node_modules/angular/angular.js';
+  res.send(fs.readFileSync(path, 'utf8'));
+});
 
 app.get('/client-token', function(req, res) {
   var fakeClientToken = JSON.stringify({
-    fake: true
+    fake: true,
+    authUrl: '',
+    clientApiUrl: ''
   });
-  // TODO: base64encode fakeClientToken
+  fakeClientToken = new Buffer(fakeClientToken).toString('base64');
   res.send(fakeClientToken);
 });
 
