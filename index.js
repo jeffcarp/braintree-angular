81926644c4db56725041e50c9796e2ebef1ff6e9
Jeff Carpenter <gcarpenterv@gmail.com>
2016-06-05 14:31:03 -0400

    Refactor components into factory

--- index.js
+++ index.js
@@ -3,84 +3,51 @@
 var braintreeWeb = require('braintree-web')
 var braingular = window.angular.module('braintree-angular', [])
 
-braingular.component('braintreeDropin', {
-  template: '<div class="braintree-dropin"></div>',
-  bindings: {
-    tokenizationKey: '@',
-    onPaymentMethodReceived: '&'
-  },
-  controller: function ($element) {
-    var instance
+braingular.component('braintreeDropin', braintreeComponent('dropin'))
+braingular.component('braintreePaypal', braintreeComponent('paypal'))
 
-    this.$onInit = function () {
-      var self = this
+function braintreeComponent (_integrationType) {
+  return {
+    template: '<div class="braintree-' + _integrationType + '"></div>',
+    bindings: {
+      tokenizationKey: '@',
+      clientToken: '@',
+      onPaymentMethodReceived: '&'
+    },
+    controller: function ($element) {
+      var instance
 
-      if (!this.tokenizationKey) {
-        console.error('requires tokenizationKey')
-        return
+      this.$onInit = function () {
+        var self = this
+        var authorization = this.tokenizationKey || this.clientToken
+
+        if (!authorization) {
+          console.error('requires tokenizationKey or clientToken')
+          return
+        }
+
+        braintreeWeb.setup(this.tokenizationKey, _integrationType, {
+          container: $element[0],
+          onReady: function (integration) {
+            instance = integration
+          },
+          onPaymentMethodReceived: function (payload) {
+            if (self.onPaymentMethodReceived) {
+              self.onPaymentMethodReceived(payload)
+            }
+          }
+        })
       }
 
-      braintreeWeb.setup(this.tokenizationKey, 'dropin', {
-        container: $element[0],
-        onReady: function (integration) {
-          instance = integration
-        },
-        onPaymentMethodReceived: function (payload) {
-          if (self.onPaymentMethodReceived) {
-            self.onPaymentMethodReceived(payload)
-          }
+      this.$onDestroy = function () {
+        if (instance) {
+          instance.teardown(function () {
+            instance = null
+          })
         }
-      })
-    }
-
-    this.$onDestroy = function () {
-      if (instance) {
-        instance.teardown(function () {
-          instance = null
-        })
       }
     }
   }
-})
-
-braingular.component('braintreePaypal', {
-  template: '<div class="braintree-paypal"></div>',
-  bindings: {
-    tokenizationKey: '@',
-    onPaymentMethodReceived: '&'
-  },
-  controller: function ($element) {
-    var instance
-
-    this.$onInit = function () {
-      var self = this
-
-      if (!this.tokenizationKey) {
-        console.error('requires tokenizationKey')
-        return
-      }
-
-      braintreeWeb.setup(this.tokenizationKey, 'paypal', {
-        container: $element[0],
-        onReady: function (integration) {
-          instance = integration
-        },
-        onPaymentMethodReceived: function (payload) {
-          if (self.onPaymentMethodReceived) {
-            self.onPaymentMethodReceived(payload)
-          }
-        }
-      })
-    }
-
-    this.$onDestroy = function () {
-      if (instance) {
-        instance.teardown(function () {
-          instance = null
-        })
-      }
-    }
-  }
-})
+}
 
 module.exports = braingular
